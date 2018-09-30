bc0853a798ceae23e99b333b0b7a22370e01e847
KVN <kvndesign.mail@gmail.com>
2018-09-30 12:47:14 +0200

    Improved message display

--- frontend/src/components/messages/MessagesPanel.js
+++ frontend/src/components/messages/MessagesPanel.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Table } from 'semantic-ui-react';
+import { Container, Table, Divider, Header } from 'semantic-ui-react';
 
 class MessagesPanel extends Component {
   constructor(props) {
@@ -12,59 +12,75 @@ class MessagesPanel extends Component {
   render() {
     return (
       <Container>
-        <br />
-        <Table celled structured>
+        { this.state.messages != null && Object.keys(this.state.messages).map(property => {
+          return (
+            this.getCells(property)
+          );
+        })}
+      </Container>
+    );
+  };
+
+  
+
+  getCells = (property) => {
+    if (this.state.messages[property].length === 0) {
+      return (
+        this.getEmptyTable(property)
+      );
+    } else {
+      return (
+        <Container key={ property }>
+          <Divider hidden/><Divider hidden={ property === '0' }/>
+          <Header as='h3'>Partition: { property }</Header>
+          <Table celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell width={2}>Offset</Table.HeaderCell>
+                <Table.HeaderCell width={2}>Timestamp</Table.HeaderCell>
+                <Table.HeaderCell width={2}>Key</Table.HeaderCell>
+                <Table.HeaderCell>Message</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+            { this.state.messages[property].map(element => {
+              return (
+                <Table.Row key={element.offset}>
+                  <Table.Cell>{element.offset}</Table.Cell>
+                  <Table.Cell>{element.timestamp}</Table.Cell>
+                  <Table.Cell>{element.key}</Table.Cell>
+                  <Table.Cell>{element.message}</Table.Cell>
+                </Table.Row>
+              )
+            })}
+            </Table.Body>
+          </Table>
+        </Container>
+      );
+    }
+  };
+
+  getEmptyTable = (property) => {
+    return (
+      <Container key={ property }>
+        <Divider hidden/><Divider />
+        <Header as='h3'>Partition: { property }</Header>
+        <Table celled>
           <Table.Header>
             <Table.Row>
-              <Table.HeaderCell>Partition</Table.HeaderCell>
-              <Table.HeaderCell>Offset</Table.HeaderCell>
-              <Table.HeaderCell>Timestamp</Table.HeaderCell>
-              <Table.HeaderCell>Key</Table.HeaderCell>
+              <Table.HeaderCell width={2}>Offset</Table.HeaderCell>
+              <Table.HeaderCell width={2}>Timestamp</Table.HeaderCell>
+              <Table.HeaderCell width={2}>Key</Table.HeaderCell>
               <Table.HeaderCell>Message</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
 
           <Table.Body>
-            { this.state.messages != null && Object.keys(this.state.messages).map(property => {
-              return (
-                  this.getCells(property)
-              );
-            })}
           </Table.Body>
         </Table>
-        
       </Container>
-    );
-  };
-
-  getCells = (property) => {
-    if (this.state.messages[property].length === 0) {
-      return (
-        <Table.Row>
-          <Table.Cell>{ property }</Table.Cell>
-          <Table.Cell />
-          <Table.Cell />
-          <Table.Cell />
-          <Table.Cell />
-        </Table.Row>
-      );
-    } else {
-      //console.log(this.state.messages[property]);
-      return (
-        this.state.messages[property].map(element => {
-          console.log(element);
-          return (
-            <Table.Row>
-              <Table.Cell>{element.partition}</Table.Cell>
-              <Table.Cell>{element.offset}</Table.Cell>
-              <Table.Cell>{element.timestamp}</Table.Cell>
-              <Table.Cell>{element.key}</Table.Cell>
-              <Table.Cell>{element.message}</Table.Cell>
-            </Table.Row>
-          )
-        })
-      );
-    }
+    )
   };
 
   componentDidMount() {
